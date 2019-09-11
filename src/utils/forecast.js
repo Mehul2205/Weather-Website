c2fb10630aa14b34bcc9fe945b602554432bb82a
Mehul Patni <mehulpatni.2205@gmail.com>
2019-09-11 23:26:25 +0530

    updated content_02

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (longitude, latitude, callback) =>{
-    const url = 'https://api.darksky.net/forecast/99a27564f320d4248fc073c16582f292/'+ encodeURIComponent(latitude) +','+ encodeURIComponent(longitude)
+    const url = 'https://api.darksky.net/forecast/99a27564f320d4248fc073c16582f292/'+ encodeURIComponent(latitude) +','+ encodeURIComponent(longitude)+'?units=si'
 
     request({url, json:true},(error, {body}) =>{
         if(error){
@@ -9,7 +9,7 @@ const forecast = (longitude, latitude, callback) =>{
         }else if(body.error){
             callback("Unable yo find location", undefined)
         }else{
-            callback(undefined, body.daily.data[0].summary+' It is currently '+ body.currently.temperature+ ' degrees out. There is '+ body.currently.precipProbability +'% chances of rain.')
+            callback(undefined, body.daily.data[0].summary+' It is currently '+ body.currently.temperature+ ' degree celcius out. There is '+ body.currently.precipProbability +'% chances of rain.')
             }
         // console.log(response.body.currently)
     })
